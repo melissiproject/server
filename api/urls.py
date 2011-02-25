0cd6642af3aaa1e414db8e5f78db1946886d9e70
Giorgos Logiotatidis <seadog@sealabs.net>
2011-02-25 15:01:40 +0200

    updated urls

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
+from django.views.generic.simple import redirect_to
 
-# from melisi.api.handlers import DropletHandler, DropletHistoryHandler, \
-#      RevisionHandler, RevisionObjectHandler, \
-#      CellHandler, CellShareHandler, \
-#      UserHandler
 
 from melisi.api.handlers import CellHandler, CellShareHandler, \
      DropletHandler, RevisionHandler, RevisionContentHandler, \
@@ -21,24 +18,24 @@ revision_content_handler = Resource(RevisionContentHandler, authentication=basic
 revision_patch_handler = Resource(RevisionPatchHandler, authentication=basic_auth)
 user_handler = Resource(UserHandler, authentication=basic_auth)
 
+urlpatterns = patterns(
+    '',
+    (r'^cell/(?P<cell_id>\w+)/$', cell_handler),
+    (r'^cell/(?P<cell_id>\w+)/share/$', cell_share_handler),
+    (r'^cell/$', cell_handler),
 
-urlpatterns = patterns('',
-                       (r'^cell/(?P<cell_id>\w+)/$', cell_handler),
-                       (r'^cell/(?P<cell_id>\w+)/share/$', cell_share_handler),
-                       (r'^cell/$', cell_handler),
+    (r'^droplet/(?P<droplet_id>\w+)/$', droplet_handler),
+    # (r'^droplet/(?P<droplet_id>\d+)/history/$', droplet_history_handler),
+    (r'^droplet/(?P<droplet_id>\w+)/revision/$', revision_handler),
+    (r'^droplet/(?P<droplet_id>\w+)/revision/latest/$', revision_handler),
+    (r'^droplet/(?P<droplet_id>\w+)/revision/latest/content/$', revision_content_handler),
+    (r'^droplet/(?P<droplet_id>\w+)/revision/latest/patch/$', revision_patch_handler),
+    (r'^droplet/(?P<droplet_id>\w+)/revision/(?P<revision_id>\d+)/$', revision_handler),
+    (r'^droplet/(?P<droplet_id>\w+)/revision/(?P<revision_id>\d+)/content/$', revision_content_handler),
+    (r'^droplet/(?P<droplet_id>\w+)/revision/(?P<revision_id>\d+)/patch/$', revision_patch_handler),
+    (r'^droplet/$', droplet_handler),
 
-                       (r'^droplet/(?P<droplet_id>\w+)/$', droplet_handler),
-                       # (r'^droplet/(?P<droplet_id>\d+)/history/$', droplet_history_handler),
-                       (r'^droplet/(?P<droplet_id>\w+)/revision/$', revision_handler),
-                       (r'^droplet/(?P<droplet_id>\w+)/revision/latest/$', revision_handler),
-                       (r'^droplet/(?P<droplet_id>\w+)/revision/latest/content/$', revision_content_handler),
-                       (r'^droplet/(?P<droplet_id>\w+)/revision/latest/patch/$', revision_patch_handler),
-                       (r'^droplet/(?P<droplet_id>\w+)/revision/(?P<revision_id>\d+)/$', revision_handler),
-                       (r'^droplet/(?P<droplet_id>\w+)/revision/(?P<revision_id>\d+)/content/$', revision_content_handler),
-                       (r'^droplet/(?P<droplet_id>\w+)/revision/(?P<revision_id>\d+)/patch/$', revision_patch_handler),
-                       (r'^droplet/$', droplet_handler),
-
-                       (r'^user/(?P<username>\w+)/$', user_handler),
-                       # (r'^user/cells/$', user_cell_handler),
-                       (r'^user/$', user_handler),
-                       )
+    (r'^user/(?P<username>\w+)/$', user_handler),
+    # (r'^user/cells/$', user_cell_handler),
+    (r'^user/$', user_handler),
+    )
