92141ec1869441fb4ea57bfde8c5a7d2d9173d68
Giorgos Logiotatidis <seadog@sealabs.net>
2011-06-07 23:15:57 +0300

    updated environment scripts

--- build-environment.sh
+++ build-environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 pip -E env install -r deps.pip
-pip -E env install -e git://gitorious.org/melissi/librsync.git#egg=melissi-librsync
-pip -E env install -e git://gitorious.org/melissi/django-mongopiston.git#egg=melissi-mongopiston
-pip -E env install -e git://gitorious.org/melissi/mongoengine.git#egg=melissi-mongoengine
+pip -E env install -e git://github.com/melissiproject/librsync.git#egg=librsync
+pip -E env install -e git://github.com/melissiproject/mongoengine.git#egg=mongoengine
+pip -E env install -e git://github.com/melissiproject/django-piston.git#egg=django-piston
 
 echo "You can now activate your environment by typing:"
 echo "~$ source env/bin/activate"
