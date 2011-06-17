2b723fb310f60b9bbb9a132abed879d0df923ead
Giorgos Logiotatidis <seadog@sealabs.net>
2011-06-17 22:32:59 +0300

    environment for sql databases

--- scripts/build-environment.sh
+++ scripts/build-environment.sh
@@ -2,8 +2,10 @@
 
 pip -E env install -r deps.pip
 pip -E env install -e git://github.com/melissiproject/librsync.git#egg=librsync
-pip -E env install -e git://github.com/melissiproject/mongoengine.git#egg=mongoengine
-pip -E env install -e git://github.com/melissiproject/django-piston.git#egg=django-piston
+# pip -E env install -e git://github.com/melissiproject/mongoengine.git#egg=mongoengine
+# pip -E env install -e git://github.com/melissiproject/django-piston.git#egg=django-piston
+pip -E env install -e git://github.com/mozilla/django-piston.git
+
 
 echo "You can now activate your environment by typing:"
 echo "~$ source env/bin/activate"
