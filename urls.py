2f7f2168700e1ea53610bda5a9f88f873720b5cd
Giorgos Logiotatidis <seadog@sealabs.net>
2011-06-17 22:33:28 +0300

    enable admin

--- urls.py
+++ urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
     # (r'^melisi/', include('melisi.foo.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
 
     # almighty melisi API
     (r'^api/', include('melisi.api.urls')),
