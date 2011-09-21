988aed7f3dcac60771c49498686410f58d4b519c
Giorgos Logiotatidis <seadog@sealabs.net>
2011-09-21 16:42:17 +0300

    add server_timestamp

--- melisi/mlscommon/api/v1/utils.py
+++ melisi/mlscommon/api/v1/utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.conf.urls.defaults import url as django_url, patterns
 
 from piston import resource
@@ -8,9 +10,21 @@ def api_url(pattern, *args, **kwargs):
 
     return django_url(pattern, *args, **kwargs)
 
+class EnhancedResponse(resource.Response):
+    def transform_data(self):
+        return {
+            'status_code': self.status_code,
+            'server_timestamp': datetime.now().isoformat(),
+            'error_code': self.error_code,
+            'error_message': self.error_message,
+            'form_errors': self.form_errors,
+            'data': self.data,
+            }
+
 class Resource(resource.Resource):
     def __init__(self, *args, **kwargs):
         if 'response_class' not in kwargs:
-            kwargs['response_class'] = resource.EnhancedResponse
+            kwargs['response_class'] = EnhancedResponse
 
         super(Resource, self).__init__(*args, **kwargs)
+
